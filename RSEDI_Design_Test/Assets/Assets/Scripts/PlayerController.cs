f6bf5dc54e243d3fae7f994ba1fab2635255770a
Mark <marktempini@googlemail.com>
2019-03-22 18:11:07 +0000

    Controller edits

--- RSEDI_Design_Test/Assets/Assets/Scripts/PlayerController.cs
+++ RSEDI_Design_Test/Assets/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public float jumpVelocity = 3f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
     public float rotSpeed = 5f;
     public float rot = 0f;
-    public Rigidbody2D rb2d;
+    protected Rigidbody2D rb2d;
 
     
     // Start is called before the first frame update
@@ -17,26 +21,34 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
+       
     }
    
 
-    // Update is called once per frame
     void FixedUpdate()
     {
        rot -= Input.GetAxis ("Horizontal") * rotSpeed;
         transform.eulerAngles = new Vector3(0.0f, 0.0f, rot);
+ 
+       if (Input.GetButtonDown ("Jump"))
+            Jump();
+ 
     }
 
-    public void AddTorque(float torque, ForceMode2D mode = ForceMode2D.Force);
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        if(rb2d.velocity.y < 0){
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb2d.velocity.y > 0 && !Input.GetButton ("Jump"))
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+    }
+
+    //public void AddTorque(float torque, ForceMode2D mode = ForceMode2D.Force);
 
     
 
-   /*  private void Movement(float h)
-    {
-         rot -= Input.GetAxis ("Horizontal") * rotSpeed;
-         transform.eulerAngles = new Vector3(0.0f, rot, 0.0f);
-    }
-    */
+   
 }
     
