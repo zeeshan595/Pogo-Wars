12079a70a4f7d897a0708bb06cfc4737de6defb6
Mark <marktempini@googlemail.com>
2019-03-22 15:52:19 +0000

    Add rotations

--- RSEDI_Design_Test/Assets/Assets/Scripts/PlayerController.cs
+++ RSEDI_Design_Test/Assets/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float rotSpeed = 5f;
+    public float rot = 0f;
+    public Rigidbody2D rb2d;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       rot -= Input.GetAxis ("Horizontal") * rotSpeed;
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, rot);
+    }
+
+    public void AddTorque(float torque, ForceMode2D mode = ForceMode2D.Force);
+
+    
+
+   /*  private void Movement(float h)
+    {
+         rot -= Input.GetAxis ("Horizontal") * rotSpeed;
+         transform.eulerAngles = new Vector3(0.0f, rot, 0.0f);
+    }
+    */
 }
+    
